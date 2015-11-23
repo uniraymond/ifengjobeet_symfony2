bb1e41074a5d7c9d352089cc2e6f5713820c660c
Raymond <uniraymond@gmail.com>
2015-11-23 19:28:26 +1300

    Day3 codes

---
+++ src/ifeng/JobeetBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ifeng\JobeetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('ifengJobeetBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/ifeng/JobeetBundle/ifengJobeetBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ifeng\JobeetBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ifengJobeetBundle extends Bundle
+{
+}
